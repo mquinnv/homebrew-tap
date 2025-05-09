5e36d61e4b809969528b48dc09d0125549b81b96
Michael Ventura <michael.ventura@gmail.com>
2025-05-09 15:15:25 -0400

    Update warpclip formula to version 2.1.1

--- Formula/warpclip.rb
+++ Formula/warpclip.rb
@@ -1,8 +1,8 @@
 class Warpclip < Formula
   desc "Remote-to-local clipboard integration for terminal users"
   homepage "https://github.com/mquinnv/warpclip"
-  url "https://github.com/mquinnv/warpclip/archive/refs/tags/v2.1.0.tar.gz"
-  sha256 "b6abf23633ed000a1fdc686a277ec673a176252015c590c606a4759b61cd55f5"
+  url "https://github.com/mquinnv/warpclip/archive/refs/tags/v2.1.1.tar.gz"
+  sha256 "1da8468757e966ac716fba7db048155dffd68752b362c4dad7ef24c3fe7a34d2"
   license "MIT"
   head "https://github.com/mquinnv/warpclip.git", branch: "main"
 
@@ -204,7 +204,7 @@ Host *
 
   def caveats
     <<~EOS
-      WarpClip v2.1.0 has been installed. To start the clipboard service:
+      WarpClip v2.1.1 has been installed. To start the clipboard service:
 
         brew services start warpclip
 
